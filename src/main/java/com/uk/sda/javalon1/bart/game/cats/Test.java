4fc06cd711099595a0a31c8b1171b78201fef24f
bart7801 <bart7801@o2.pl>
2019-09-29 11:30:01 +0100

    english wersion

---
+++ src/main/java/com/uk/sda/javalon1/bart/game/cats/Test.java
@@ -0,0 +1,15 @@
+package com.uk.sda.javalon1.bart.game.cats;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Test {
+
+
+    @GetMapping("/hello-world")
+    public String getHelloWorld() {
+        return "catsView";
+    }
+
+}
---
+++ src/main/java/com/uk/sda/javalon1/bart/game/game/RandomNumber.java
@@ -0,0 +1,21 @@
+package com.uk.sda.javalon1.bart.game.game;
+
+public class RandomNumber {
+    private int userValue;
+
+    public RandomNumber(int userValue) {
+        this.userValue = userValue;
+    }
+
+    public RandomNumber() {
+
+    }
+
+    public int getUserValue() {
+        return userValue;
+    }
+
+    public void setUserValue(int userValue) {
+        this.userValue = userValue;
+    }
+}
